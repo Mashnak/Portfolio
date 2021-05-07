5255ba488fb3a79d0739aefba57038224c32075b
Schmiddi2405 <markus.schmidgall@googlemail.com>
2021-05-07 13:42:03 +0200

    Create components for the header to build in angular what we just built with only HTML and CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { NavigationItemComponent } from './components/navigation-item/navigation-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    NavigationComponent,
+    NavigationItemComponent
   ],
   imports: [
     BrowserModule,
