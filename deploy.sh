ad7a138d249b2654983e4b405b0e426fa3a198c5
Markus Schmidgall <markus.schmidgall@googlemail.com>
2021-05-12 09:27:44 +0200

    Create shell script to deploy application to github.io

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#
+# Copyright (c) Markus Schmidgall 2021
+# Portfolio Projekt um Angular von Grund auf zu lernen
+#
+ng build --prod --base-href https://mashnak.github.io/Portfolio/
+
+angular-cli-ghpages -d dist/Portfolio/ --no-silent
